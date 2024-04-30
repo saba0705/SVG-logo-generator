1a74dcb7624819b97da7e495e6bd29bee16c31bd
saba pervez <sabah.junaid@outlook.com>
2024-04-29 21:47:36 -0400

    updated readme and added video link

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# SVG-logo-generator
+SVG logo Generator Challenge
+This Challenge helps us build an application to prompt the user with questions to generate a SVG logo, testing them and save the generated logo to the examples folder. 
+
+Table of Contents
+
+* Description
+* Elements
+* Installation
+* Usage
+* Test
+* License
+
+Challenge Elements
+
 User Story
 
 AS a freelance web developer
@@ -20,3 +33,27 @@ THEN an SVG file is created named `logo.svg`
 AND the output text "Generated logo.svg" is printed in the command line
 WHEN I open the `logo.svg` file in a browser
 THEN I am shown a 300x200 pixel image that matches the criteria I entered
+
+Installation Instructions
+
+* Enter git clone git@github.com:saba0705/SVG-logo-generator.git to clone the repository.
+* Install dependencies by running npm install.
+
+Usage Instructions
+
+* Run the application by executing node index.js.
+* Follow the prompts to input information about your logo (text, text color, shape, and shape color).
+* Once you've completed all the prompts, the application will generate a logo based on your inputs and save it as  logo.  svg.
+
+Test Instructions
+
+* Enter npm test into your command line.
+* This will run the setcolor.test.js, shapes.test.js, and svg.test.jsfiles.
+* After the tests have been run, the results will be printed to the command line.
+
+
+
+To view a video :
+
+* Walkthough video link "https://app.screencastify.com/v3/watch/iDlOitkSTnQoSPydCBKc"
+ 
--- index.js
+++ index.js
@@ -1,47 +1,23 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Triangle, Circle, Square } = require('./lib/shapes');
+const path = require('path');
+const { Triangle, Circle, Square, } = require('./lib/shapes');
 
-// Function to generate the SVG logo based on user input
-function generateLogo(text, textColor, shapeType, shapeColor) {
-  let shape;
-  switch (shapeType) {
-    case 'Triangle':
-      shape = new Triangle();
-      break;
-    case 'Circle':
-      shape = new Circle();
-      break;
-    case 'Square':
-      shape = new Square();
-      break;
-    default:
-      console.log('Invalid shape type.');
-      return;
-  }
 
-  shape.setColor(shapeColor);
 
-  const svgContent = `
-    <svg width="300" height="200">
-      ${shape.render()}
-      <text x="50%" y="50%" fill="${textColor}" font-size="24" text-anchor="middle">${text}</text>
-    </svg>
-  `;
-
-  fs.writeFile('logo.svg', svgContent, (err) => {
-    if (err) throw err;
-    console.log('Generated logo.svg');
-  });
-}
 
 // Prompt the user for input using Inquirer
-inquirer
-  .prompt([
+const questions = [
     {
       type: 'input',
       name: 'text',
       message: 'Enter up to three characters for the text:',
+      validate: (input) => {
+        if (input.length > 3) {
+          return 'Please enter up to three characters.';
+        }
+        return true;
+      },
     },
     {
       type: 'input',
@@ -51,7 +27,7 @@ inquirer
     {
       type: 'list',
       name: 'shapeType',
-      message: 'Choose a shape (Triangle, Circle, Square):',
+      message: 'Choose a logo shape (Triangle, Circle, Square):',
       choices: ['Triangle', 'Circle', 'Square'],
     },
     {
@@ -59,11 +35,40 @@ inquirer
       name: 'shapeColor',
       message: 'Enter the shape color (color keyword or hexadecimal number):',
     },
-  ])
-  .then((answers) => {
+  ];
+  inquirer.prompt(questions).then((answers) => {
     const { text, textColor, shapeType, shapeColor } = answers;
-    generateLogo(text, textColor, shapeType, shapeColor);
-  })
-  .catch((error) => {
-    console.log('Error:', error);
+    let shape;
+
+    
+    switch (shapeType) {
+      case 'Triangle':
+        shape = new Triangle();
+        break;
+      case 'Circle':
+        shape = new Circle();
+        break;
+      case 'Square':
+        shape = new Square();
+        break;
+      default:
+        console.log('Invalid shape type.');
+        return;
+    } 
+    shape.setColor(shapeColor);
+  
+   
+  
+  const svgContent =`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shape.render()}
+      <text x="50%" y="50%" fill="${textColor}" font-size="24" text-anchor="middle">${text}</text>
+    </svg>
+  `;
+
+  fs.writeFile(`example/logo-${shapeType}.svg`, svgContent, (err) => {
+    if (err) throw err;
+    console.log('Generated logo.svg');
   });
+}
+
+);
